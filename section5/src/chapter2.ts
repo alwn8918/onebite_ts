92f7e3655667113ce1fbd5b43d563e09dacf84de
alwn8918 <alwn8918@gmail.com>
2025-05-01 02:14:59 +0900

    [Section5] 함수 타입의 호환성

---
+++ section5/src/chapter2.ts
@@ -0,0 +1,52 @@
+// 함수 타입의 호환성
+
+// 1. 반환값이 호환
+type A = () => number;
+type B = () => 10;
+
+let a: A = () => 10;
+let b: B = () => 10;
+
+a = b;
+// b = a; // Error
+
+// 2. 매개변수가 호환
+// 2-1. 매개변수의 개수가 같을 때
+type C = (value: number) => void;
+type D = (value: 10) => void;
+
+let c: C = (value) => {};
+let d: D = (value) => {};
+
+// c = d; // Error
+d = c;
+
+type Animal = {
+  name: string;
+};
+
+type Dog = {
+  name: string;
+  color: string;
+};
+
+let animalFunc = (animal: Animal) => {
+  console.log(animal.name);
+};
+
+let dogFunc = (dog: Dog) => {
+  console.log(dog.name);
+  console.log(dog.color);
+};
+
+// animalFunc = dogFunc; // Error
+
+// 2-2. 매개변수의 개수가 다를 때
+type Func1 = (a: number, b: number) => void;
+type Func2 = (a: number) => void;
+
+let func1: Func1 = (a, b) => {};
+let func2: Func2 = (a) => {};
+
+func1 = func2;
+// func2 = func1; // Error
