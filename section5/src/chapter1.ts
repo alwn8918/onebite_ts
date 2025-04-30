9607ecb06ffa0bec891af71c92d5fea81e9b0f4b
alwn8918 <alwn8918@gmail.com>
2025-05-01 02:02:10 +0900

    [Section5] 함수 타입 표현식과 호출 시그니처

---
+++ section5/src/chapter1.ts
@@ -0,0 +1,16 @@
+type Operation = (a: number, b: number) => number;
+
+const add: Operation = (a, b) => a + b;
+const sub: Operation = (a, b) => a - b;
+const mul: Operation = (a, b) => a * b;
+const div: Operation = (a, b) => a / b;
+
+type Operation2 = {
+  (a: number, b: number): number;
+  name: string;
+};
+
+const add2: Operation2 = (a, b) => a + b;
+const sub2: Operation2 = (a, b) => a - b;
+const mul2: Operation2 = (a, b) => a * b;
+const div2: Operation2 = (a, b) => a / b;
